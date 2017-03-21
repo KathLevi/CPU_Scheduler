b5c58e1ea7308930c4b2d972ecf2b7c322dfa482
Jarek Thomas <jthomas17@my.whitworth.edu>
2017-03-21 11:25:17 -0700

    Added comments and started srt algorithm

---
+++ CPU_Scheduler-master/OSsimulation/OSsimulation/FCFS.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Diagnostics;
+
+namespace OSsimulation
+{
+    class FCFS
+    { 
+        /**List of processes that will be ran through algorithm*/
+        public List<Process> fcfs = new List<Process>();
+
+        /**Stopwatch that will hold the total time that the process
+         * is created to the time that it is terminated*/
+        Stopwatch Stopwatch = new Stopwatch();
+
+        /** Constructor
+        \param number of processes to add to the list*/
+        public FCFS(int num)
+        {
+            Stopwatch.Start();
+            for(int i = 0; i < num; ++i)
+            {
+                Process process = new Process();
+                //Set each processes enter queue time upon creation and addition to the queue
+                process.time_enter_queue = Stopwatch.Elapsed.Seconds;
+
+                fcfs.Add(process);
+
+            }
+        }
+
+        /**Loops through the process that is first in the list till it is done*/
+        public async void Run()
+        {
+            /* Loop through processes in queue starting at the back of the queue where the shortest process will be next*/
+            for (int j = fcfs.Count(); j > 0; --j)
+            {
+                //Gets the time that the stop watch has been running. This is the time that the process has been on the queue
+                fcfs[0].time_start = Stopwatch.Elapsed.Seconds;
+                for (int i = fcfs[0].time_to_run; i > 0; --i)
+                {
+                    //Just loop through till time hits zero, doesnt really have to do anything while running
+                    //Need to add burst times for when process is on cpu and in i/o
+                }
+                
+                //For math before popped off queue
+                fcfs[0].time_end = Stopwatch.Elapsed.Seconds;
+                //This is where you record stuff
+                //Add some function
+
+                fcfs.RemoveAt(0);
+               
+            }
+            Stopwatch.Stop();
+        }
+
+    }
+}
---
+++ CPU_Scheduler-master/OSsimulation/OSsimulation/Process.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSsimulation
+{
+    class Process
+    {
+        /**Total time for the process to run*/
+        public int time_to_run = 0;
+        /** Time that the process requires on the cpu*/
+        public int time_on_cpu = 0;
+        /**Total time that the process will take in i/o*/
+        public int time_in_io = 0;
+        /**Total time that the process is in the waiting queue*/
+        public int time_in_queue = 0;
+
+        /**Time the processor entered the queue*/
+        public int time_enter_queue;
+        /**Time that the process was created*/
+        public int time_start;
+        /**Time that the process terminates*/
+        public int time_end;
+
+        /**Constructor that assignes a random time to time_in_io and time_on_cpu
+         * Then sets time_to_tun to the sum of those two vallues*/
+        public Process()
+        {
+            //initializing random
+            Random rand = new Random();
+            //generating between 1 and 50000 for time to run i/o and time on cpu
+            time_in_io = rand.Next(1, 50000);
+            time_on_cpu = rand.Next(1, 50000);
+            //Make sure that total time is not greater or less than time to run i/o and time on cpu combined
+            time_to_run = time_in_io + time_on_cpu;
+        }
+
+    }
+}
