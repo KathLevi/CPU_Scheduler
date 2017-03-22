47cbf856c13bb77d77f0cbb5ce146142300805c3
agazeley <andrew.gazeley@gmail.com>
2017-03-22 15:22:52 -0700

    Still broken but not as sad

---
+++ OSsimulation 1.2/OSsimulation/Algs/SRT.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSsimulation
+{
+    //This one is the only premptive one
+    class SRT
+    {
+        List<Process> srt = new List<Process>();
+
+        public SRT(int num_processes)
+        {
+            for(int i = 0; i < num_processes; ++i)
+            {
+                Process process = new Process();
+                if (srt.Count == 0)
+                    srt.Add(process);
+                else
+                {
+                    for(int j = srt.Count; j > -1; --j)
+                    {
+                        if (process.time_to_run <= srt[j].time_to_run)
+                        {
+                            srt.Insert(j, process);
+                        }
+                        else if (j == srt.Count)
+                            srt.Add(process);
+                    }
+                }
+            }
+        }
+    }
+}
