f65e0d62d1bf209b52f0950689d27f7e7a0ecbe9
Jarek Thomas <jthomas17@my.whitworth.edu>
2017-03-23 10:46:56 -0700

    stuff was done that nworked
    MFQ is done and working

---
+++ OSsimulation 1.6/OSsimulation/RoundRobin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace OSsimulation
+{
+    class RoundRobin
+    {
+        /**container for all the proccesses that we are running.*/
+        List<Process> roundr;
+
+        Stopwatch Stopwatch = new Stopwatch();
+
+        /**constructor
+        \param Process to be added to the List*/
+        public RoundRobin(Process process)
+        {
+            roundr.Add(process);
+        }
+
+        /**runs through the algorithm until all processes are done.*/
+        public void Run()
+        {
+            //runs the process for 20 ticks and updates time them puts it to the back of the List
+            for(int i = roundr[0].time_to_run; i > 0; i-=1)
+            {
+                if (i == 20)
+                    roundr[0].time_to_run -= i;
+                    break;
+            }
+            //puts current process to the back of the queue to wait its turn again
+            if (roundr[0].time_to_run > 0)
+            {
+                roundr.Add(roundr[0]);
+                roundr.RemoveAt(0);
+            }
+            //if its done then remove it
+            else
+            {
+                roundr.RemoveAt(0);
+            }
+
+        }
+    }
+}
